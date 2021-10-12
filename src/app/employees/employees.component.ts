ff7caa76dce7fd79c4a3f95c5f103030c3bdd262
caglarcercinli <59973993+caglarcercinli@users.noreply.github.com>
2021-10-12 16:13:28 +0200

    more style

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Employee } from '../employee';
+import { EMPLOYEES } from '../mock-employees';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
-  employee : Employee={
-    id: 1,
-    firstName: 'Caglar',
-    lastName:'Cercinli',
-    email:'caglar@mail.com',
-    password:'caglar'
-};
+  employees = EMPLOYEES;
+//   employee : Employee={
+//     id: 1,
+//     firstName: 'Caglar',
+//     lastName:'Cercinli',
+//     email:'caglar@mail.com',
+//     password:'caglar'
+// };
+  selectedEmployee?: Employee;
+  onSelect(employee: Employee): void {
+    this.selectedEmployee = employee;
+  }
 
   constructor() { }
 
---
+++ src/app/mock-employees.ts
@@ -0,0 +1,7 @@
+import { Employee } from './employee';
+export const EMPLOYEES: Employee[] = [
+  { id: 11, firstName: 'caglar',lastName:'cercinli',
+    email:'caglar@mail.com',password:'caglar' },
+  { id: 12, firstName: 'john',lastName:'white',
+    email:'john@mail.com',password:'john' },
+];
